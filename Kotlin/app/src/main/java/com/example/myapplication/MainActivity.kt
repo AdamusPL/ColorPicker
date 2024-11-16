4d1e203c9dad8fc491a31f068dafdf9fca45c490
Piotr Kuchnowski <kuchnowski.piotr@outlook.com>
2024-11-16 16:09:58 +0100

    Chyba git

--- Kotlin/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ Kotlin/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,14 +6,16 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -22,7 +24,15 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -31,6 +41,7 @@ import com.example.myapplication.ui.components.ColorWheel
 import com.example.myapplication.ui.components.Square
 import com.example.myapplication.ui.components.TextFieldRow
 import com.example.myapplication.ui.theme.MyApplicationTheme
+
 
 class MainActivity : ComponentActivity() {
     private val viewModel: MainViewModel by viewModels()
@@ -64,8 +75,30 @@ fun FirstScreen(viewModel : MainViewModel, innerPadding: PaddingValues) {
 
 @Composable
 fun SecondScreen(viewModel: MainViewModel, innerPadding: PaddingValues){
-    Column (modifier = Modifier.padding(innerPadding)) {
-        ColorWheel(viewModel)
+    Column(
+        modifier = Modifier
+            .padding(innerPadding)
+            .fillMaxSize()
+    ) {
+        Box(
+            modifier = Modifier.weight(1f).fillMaxWidth(),
+            contentAlignment = Alignment.Center
+
+        ) {
+            ColorWheel(viewModel)
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxWidth(),
+            contentAlignment = Alignment.Center
+        ) {
+            Box(
+                modifier = Modifier.size(100.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Square(viewModel)
+            }
+        }
         TextFieldRow(viewModel.rgbFieldValues, listOf("R", "G", "B"), viewModel, true)
         TextFieldRow(viewModel.hsvFieldValues, listOf("H", "S", "V"), viewModel, true)
         TextFieldRow(viewModel.cmykFieldValues, listOf("C", "M", "Y", "K"), viewModel, true)
@@ -74,18 +107,70 @@ fun SecondScreen(viewModel: MainViewModel, innerPadding: PaddingValues){
 }
 
 @Composable
-fun BottomNavigationBar(navController : NavHostController) {
-    BottomAppBar {
-        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
-            IconButton(onClick = { navController.navigate("first") }) {
-                Icon(Icons.Filled.Home, contentDescription = "Home")
+fun BottomNavigationBar(navController: NavHostController) {
+    var selectedIndex by remember { mutableIntStateOf(0) }
+
+    BottomAppBar(
+        containerColor = Color.White,
+        contentColor = Color.Gray,
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceAround
+        ) {
+            IconButton(
+                modifier = Modifier.size(96.dp),
+                onClick = {
+                    selectedIndex = 0
+                    navController.navigate("first") {
+                        popUpTo("first") { inclusive = true }
+                        launchSingleTop = true
+                    }
+                },
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.calculate),
+                        contentDescription = "Konwerter",
+                        tint = if (selectedIndex == 0) Color(0xFFFFC107) else Color.Gray,
+                    )
+                    Text(
+                        text = "Konwerter",
+                        color = if (selectedIndex == 0) Color(0xFFFFC107) else Color.Gray,
+                    )
+                }
             }
-            IconButton(onClick = { navController.navigate("second") }) {
-                Icon(Icons.Filled.Home, contentDescription = "Home")
+            IconButton(
+                modifier = Modifier.size(96.dp),
+                onClick = {
+                    selectedIndex = 1
+                    navController.navigate("second") {
+                        popUpTo("second") { inclusive = true }
+                        launchSingleTop = true
+                    }
+                },
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.palette),
+                        contentDescription = "Paleta",
+                        tint = if (selectedIndex == 1) Color(0xFFFFC107) else Color.Gray,
+                    )
+                    Text(
+                        text = "Paleta",
+                        color = if (selectedIndex == 1) Color(0xFFFFC107) else Color.Gray,
+                    )
+                }
             }
         }
     }
 }
+
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
